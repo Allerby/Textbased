72e542b8e4634dd805e132b0bfd11931e76bf83f
Michael Lee Allerby <michael.allerby@designworks.co.nz>
2014-07-20 01:52:10 +1200

    Customised Login Screen
    Added fonts + customised login screen

--- wp-content/themes/textbased/functions.php
+++ wp-content/themes/textbased/functions.php
@@ -1,13 +1,20 @@
 <?php
 function textbased_enqueue_scripts() {
   $url = get_template_directory_uri();
-  
+
   // Styles
-  wp_enqueue_style('style', $url . '/style.css', array(), '1.1', 'screen');
-  
+  wp_enqueue_style('style', $url . '/style.css');
+  // wp_enqueue_style('style', $url . '/login/login-styles.css');
+
   // Javascript
   wp_deregister_script('jquery');
   wp_enqueue_script('jquery', $url . '/js/jquery.js', array(), '1.9.1');
   wp_enqueue_script('script', $url . '/js/script.js', array('jquery'), '1.0');
 }
 add_action('wp_enqueue_scripts', 'textbased_enqueue_scripts');
+
+  //Custom login css
+function my_login_stylesheet() {
+  wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/login/login-styles.css' );
+}
+add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
--- wp-content/themes/textbased/index.php
+++ wp-content/themes/textbased/index.php
@@ -1,3 +1,7 @@
 <?php get_header(); ?>
 
+
+  <div class="btn-su">
+    <h1>Text Based<span style="font-weight:600;">.<span></h1>
+  </div>
 <?php get_footer(); ?>
